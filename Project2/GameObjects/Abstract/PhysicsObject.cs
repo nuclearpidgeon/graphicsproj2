c0baaa0852f3bd03db77fda923a8cb05b0b77ee4
Stewart Webb <stewart.james.webb@gmail.com>
2014-10-21 00:30:57 +1100

    added player check, but it doesn't work yet :(

--- Project2/GameObjects/Abstract/PhysicsObject.cs
+++ Project2/GameObjects/Abstract/PhysicsObject.cs
@@ -63,6 +63,11 @@ namespace Project2.GameObjects.Abstract
             {
                 Destroy();
                 game.RemoveGameObject(this);
+                if (Object.ReferenceEquals(this, this.game.player))
+                {
+                    // this doesn't work
+                    game.level.ResetPlayer();
+                }
                 return;
             }
             // each call to SetX recalculates the world matrix. This is inefficient and should be fixed.
